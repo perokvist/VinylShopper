bb86e562ab5d7c528f8c511671170cf03179e9be
Andreas Hammar <andreas.hammar@jayway.com>
2013-04-22 23:29:07 +0200

    design vm

---
+++ src/Clients/VinylShopper.Domain/ViewModels/ResultDesign.cs
@@ -0,0 +1,16 @@
+﻿namespace VinylShopper.Domain.ViewModels
+{
+    public class ResultDesign : Result
+    {
+        public ResultDesign()
+        {
+            Title = "A long way home";
+            Artist = "Fleetwood Mac";
+            Label = "A records";
+            Price = "7,95 E";
+            Cover = "ApplicationIcon.png";
+            Format = "12\"";
+            Pressing = "Original";
+        }
+    }
+}
